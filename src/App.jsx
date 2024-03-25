41509fa01a9828ba85801a3b30f0cbe3805427b1
Julia Souza <julia.souza18@eaportal.org>
2024-03-25 18:59:42 -0300

    primeiro commit da responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import iconLuxury from "./assets/icon-luxury.svg"
 
 export default function App(){
   return(
-    <main className="min-h-svh bg-offwhitebg py-20 px-6">
-      <section>
+    <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center">
+      <section className="lg:flex">
         <div className="bg-orange p-12 rounded-t-lg">
           <img src={iconSedans} alt="Ícone dos carros sedan" className="mb-7" />
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg  ">Sedans</h2>
@@ -14,14 +14,14 @@ export default function App(){
             <a className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-orange " href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
         </div>
 
-        <div className="bg-green p-12">
+        <div className="bg-green p-12 antialiased">
           <img src={iconSuvs} alt="Ícone dos carros SUV " className="mb-7" />
           <h2 className="font-shoulders mb-7 uppercase text-4xl  font-bold text-offwhitebg">SUVs</h2>
           <p className="font-lexend mb-7 text-offwhite">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
           <a className= "font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-green"href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
         </div>
 
-        <div className="bg-dark-green p-12 rounded-b-lg">
+        <div className="bg-dark-green p-12 rounded-b-lg antialiased">
           <img src={iconLuxury} alt="Ícone dos carros de luxo." className="mb-7" />
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Luxuosos</h2>
           <p className="font-lexend mb-7 text-offwhite" >Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
